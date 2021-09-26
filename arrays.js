f3c7e9be2fcfab8c578b9a851e1b3717ae96a634
lexraye <70794561+lexraye@users.noreply.github.com>
2021-09-25 23:35:49 -0700

    added problem to solve

--- arrays.js
+++ arrays.js
@@ -1,1 +1,10 @@
 //array challenges
+
+function alwaysHungry(arr) {
+    // your code here 
+}
+   
+alwaysHungry([3.14, "food", "pie", true, "food"]);
+// this should console log "yummy", "yummy"
+alwaysHungry([4, 1, 5, 7, 2]);
+// this should console log "I'm hungry"copy
